cfd574bcfb02c8a8dfa7d792012518d774c1c825
fred <fred.m.li.93@gmail.com>
2016-12-29 15:48:12 -0800

    completed assignment

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends Bee{
   // TODO..
+  constructor() {
+  	super();
+  	this.age = 10;
+  	this.job = 'find pollen';
+  	this.canFly = true;
+  	this.treasureChest = [];
+  }
+  forage( value ) {
+  	this.treasureChest.push(value);
+  }
 };
